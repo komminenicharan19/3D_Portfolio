55b5eeed44cde4443a982319d2f3188f6bac857d
Devi_Charan <71281880+komminenicharan19@users.noreply.github.com>
2024-04-11 20:18:33 +0530

    Testimonials and Contact

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,6 +2,8 @@ import { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
+// import { Primitive } from 'react-three-fiber';
+
 
 const Computers = ({isMobile}) => {
 
